a9b19889ee17e25f7d0aef769bb9230f77324a7f
TiniT <tinnt5@vnpay.vn>
2026-03-31 16:01:27 +0700

    Initial Commit

---
+++ CineTrack/CineTrackApp.swift
@@ -0,0 +1,17 @@
+//
+//  CineTrackApp.swift
+//  CineTrack
+//
+//  Created by TiniT on 31/3/26.
+//
+
+import SwiftUI
+
+@main
+struct CineTrackApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
